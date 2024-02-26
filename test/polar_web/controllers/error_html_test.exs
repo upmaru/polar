39c06912913009af31b806b6693c2204922fac13
Zack Siri <me@zacksiri.dev>
2024-02-26 19:54:11 +0700

    Update error page test

--- test/polar_web/controllers/error_html_test.exs
+++ test/polar_web/controllers/error_html_test.exs
@@ -5,7 +5,7 @@ defmodule PolarWeb.ErrorHTMLTest do
   import Phoenix.Template
 
   test "renders 404.html" do
-    assert render_to_string(PolarWeb.ErrorHTML, "404", "html", []) == "Not Found"
+    assert render_to_string(PolarWeb.ErrorHTML, "404", "html", []) =~ "Not Found"
   end
 
   test "renders 500.html" do
