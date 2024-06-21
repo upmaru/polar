a3d7ca77fe043695b3ee6733abc663e444961abe
Zack Siri <me@zacksiri.dev>
2024-06-21 18:49:02 +0700

    Implement transit for cluster

---
+++ lib/polar/machines/cluster/transit.ex
@@ -0,0 +1,15 @@
+defimpl Eventful.Transit, for: Polar.Machines.Cluster do
+  def perform(cluster, user, event_name, options \\ []) do
+    comment = Keyword.get(options, :comment)
+    domain = Keyword.get(options, :domain, "transitions")
+    parameters = Keyword.get(options, :parameters, %{})
+
+    cluster
+    |> Event.handle(user, %{
+      domain: domain,
+      name: event_name,
+      comment: comment,
+      parameters: parameters
+    })
+  end
+end
